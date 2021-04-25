48c13530b5e41e6f26bf718a821d6e641128ee6d
luiscaleni <77455357+luiscaleni@users.noreply.github.com>
2021-04-25 12:06:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="images/icon30.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>OMEGA</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <ul>
+                <li id="m"><span class="mobile"></span></li>
+                <li><a href="#omega">Omega</a></li>
+                <li><a href="#precision">Precisiòn</a></li>
+                <li><a href="#estilo">Estilo</a></li>
+                <li><a href="#resistencia">Resistencia</a></li>
+            </ul>
+        </div>
+    </header>
+    <section class="portada1" id="omega">
+        <div class="titulo1">
+            <h1 id="e">OMEGA</h1>
+            <h1 class="mobile">O<br>M<br>E<br>G<br>A</h1>
+            <span>Watch</span>
+        </div>
+    </section>
+
+    <section class="portada2" id="precision">
+        <div class="titulo2">
+            <h1>Precision</h1>
+            <span>OMEGA Watch</span>
+        </div>
+    </section>
+
+    <section class="portada3" id="estilo">
+        <div class="titulo3">
+            <h1>Estilo</h1>
+            <span>OMEGA Watch</span>
+        </div>
+    </section>
+
+    <section class="portada4" id="resistencia">
+        <div class="titulo4">
+            <h1>Resistencia</h1>
+            <span>OMEGA Watch</span>
+        </div>
+    </section>
+
+    <footer>
+        <div class="contenedor">
+            ©Copyright - Responsive - Contactar: <a href="mailto:luis.caleni@gmail.com">Luis Caleni</a>
+        </div>
+    </footer>
+</body>
+</html>
